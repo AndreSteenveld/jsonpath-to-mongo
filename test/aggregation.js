b2d7df6de2286f5d8d0c6908271c98969b544f9a
Andre Steenveld <andre.steenveld@gmail.com>
2018-06-27 18:42:28 +0200

    Add the tests for logical operations

--- test/aggregation.js
+++ test/aggregation.js
@@ -12,13 +12,40 @@ function mongo_aggregation( assert, path, expection ){
 const path_pipeline = {
 
     "$[?( @.property === 37 )]": [{ $match: { "property" : { $eq : 37 } } }],
+    "$[?( @.property.is.nested.deeply === 37 )]": [{ $match: { "property.is.nested.deeply" : { $eq : 37 } } }],
+    "$.property[?( @.is.nested === 37 )]": [{ $match: { "property.is.nested" : { $eq : 37 } } }],
     
     "$.property[?( @ === 37 )]": [{ $match: { "property" : { $eq : 37 } } }],
     "$.property[?( @ !== 37 )]": [{ $match: { "property" : { $neq : 37 } } }],
 
     "$.property[?( @ < 37 )]": [{ $match: { "property" : { $lt : 37 } } }],
     "$.property[?( @ <= 37 )]": [{ $match: { "property" : { $lte : 37 } } }],
+    
+    "$[?( @.have == 'cake' && @.eat == 'it' )]": [
 
+        { $match: {
+
+            $and: [
+                { "have" : { $eq : "cake" } },
+                { "eat" : { $eq : "it" } }
+            ]
+
+        }}
+
+    ],
+
+    "$[?( @.the == 'one' || @.the == 'other' )]": [
+
+        { $match: {
+
+            $or: [
+                { "the" : { $eq : "one" } },
+                { "the" : { $eq : "other" } }
+            ]
+
+        }}
+
+    ]
 };
 
 for( const [ path, pipeline ] of Object.entries( path_pipeline ) )
