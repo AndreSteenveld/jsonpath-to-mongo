93e9d86b41574f211da20d14444b6cf7787257f6
Andre Steenveld <andre.steenveld@gmail.com>
2018-08-03 01:16:46 +0200

    Remove to_find

--- test/transform.js
+++ test/transform.js
@@ -2,7 +2,7 @@ import test from "ava";
 import { to_aggregation, to_find } from "../src";
 
 test( "to_aggregation is a function", ( assert ) => assert.true( "function" === typeof to_aggregation ) );
-test( "to_find is a function", ( assert ) => assert.true( "function" === typeof to_find ) );
+
 
 
 
